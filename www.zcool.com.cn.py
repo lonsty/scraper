3ea2f1892a65b75b79166659506d8d3ad8432493
lonsty <lonsty@sina.com>
2019-09-08 02:01:26 +0800

    ojbk version

--- www.zcool.com.cn.py
+++ www.zcool.com.cn.py
@@ -2,7 +2,7 @@
 # @Author: Allen
 # @Date:   2019-09-07 18:34:18
 # @Last Modified by:   Allen
-# @Last Modified time: 2019-09-07 22:56:48
+# @Last Modified time: 2019-09-08 02:00:21
 import os
 import re
 import sys
@@ -11,13 +11,13 @@ from concurrent.futures import ThreadPoolExecutor, wait
 from datetime import datetime
 from functools import wraps
 from queue import Empty, Queue
+from threading import Thread
 from time import sleep
 from urllib.parse import urljoin, urlparse
 from uuid import uuid4
 
 import click
 import requests
-
 from bs4 import BeautifulSoup
 
 Scrapy = namedtuple('Scrapy', 'type author title url')
@@ -25,15 +25,19 @@ HOST_PAGE = 'https://www.zcool.com.cn'
 PAGE_SUFFIX = '?myCate=0&sort=1&p={page}'
 USER_SUFFIX = '/u/{id}'
 SEARCH_DESIGNER_SUFFIX = '/search/designer?&word={word}'
+TIMEOUT = (5, 10)
 
 
-def print_status(func):
+def update_result(func):
     @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        result = func(self, *args, **kwargs)
-        print(f'Time spent: {str(datetime.now() - self.start_time)[:-7]} \tRemaining tasks: '
-              f'{self.to_crawl.qsize():5d} \tCompleted tasks: {len(self.scraped):5d}', end='\r')
-        return result
+    def wrapper(self, scrapy, *args, **kwargs):
+        try:
+            result = func(self, scrapy, *args, **kwargs)
+            self.scraped.add(scrapy)
+        except Exception:
+            self.failed.add(scrapy)
+        else:
+            return result
 
     return wrapper
 
@@ -41,16 +45,21 @@ def print_status(func):
 class MultiThreadScraper():
 
     def __init__(self, user_id=None, username=None, directory=None, max_pages=None, max_topics=None, max_workers=None):
+        self.start_time = datetime.now()
+        print(self.start_time.ctime())
+
         self.user_id = user_id or self.search_id_by_username(username)
         self.max_topics = max_topics or 'all'
         self.max_workers = max_workers or 20
         self.base_url = HOST_PAGE + USER_SUFFIX.format(id=self.user_id)
         self.pool = ThreadPoolExecutor(self.max_workers)
         self.scraped = set([])
+        self.failed = set([])
         self.to_crawl = Queue()
 
-        response = requests.get(self.base_url, timeout=(5, 10))
+        response = requests.get(self.base_url, timeout=TIMEOUT)
         soup = BeautifulSoup(markup=response.text, features='html.parser')
+
         try:
             author = soup.find(name='div', id='body').get('data-name')
             if username and username != author:
@@ -78,9 +87,8 @@ class MultiThreadScraper():
         print(f'Max pages: {max_page}')
         print(f'Pages to scrapy: {self.max_pages}')
         print(f'Topics to scrapy: {"all" if self.max_pages == "all" else (self.max_pages * self.max_topics)}')
-        print(f'Storage directory: {self.directory}')
-        self.start_time = datetime.now()
-        print(self.start_time.ctime(), end='\n\n')
+        print(f'Storage directory: {self.directory}', end='\n\n')
+        Thread(target=self.show_task_status, args=(0.5,), daemon=True).start()  # background task to update status.
 
     @staticmethod
     def mkdirs_if_not_exist(dir):
@@ -99,7 +107,7 @@ class MultiThreadScraper():
         if not username:
             raise ValueError('Must give a <user id> or <username>!')
         try:
-            response = requests.get(urljoin(HOST_PAGE, SEARCH_DESIGNER_SUFFIX.format(word=username)), timeout=(5, 10))
+            response = requests.get(urljoin(HOST_PAGE, SEARCH_DESIGNER_SUFFIX.format(word=username)), timeout=TIMEOUT)
         except Exception:
             print(f'Failed to connect to {HOST_PAGE}')
             sys.exit(1)
@@ -112,24 +120,31 @@ class MultiThreadScraper():
         id = author_1st.get('data-id')
         return id
 
-    @print_status
+    def show_task_status(self, interval=None):
+        print(f'Time spent: {str(datetime.now() - self.start_time)[:-7]} \tRemaining tasks: '
+              f'{self.to_crawl.qsize():5d} \tCompleted tasks: {len(self.scraped):5d}', end='\r', flush=True)
+        while interval:
+            sleep(interval)
+            print(f'Time spent: {str(datetime.now() - self.start_time)[:-7]} \tRemaining tasks: '
+                  f'{self.to_crawl.qsize():5d} \tCompleted tasks: {len(self.scraped):5d}', end='\r', flush=True)
+
+    @update_result
     def scrape_page(self, scrapy):
         try:
-            res = requests.get(scrapy.url, timeout=(3, 10))
+            res = requests.get(scrapy.url, timeout=TIMEOUT)
             return scrapy, res
         except requests.RequestException:
-            return
+            return scrapy, None
 
-    @print_status
+    @update_result
     def parse_topics(self, scrapy, html):
         soup = BeautifulSoup(markup=html.text, features='html.parser')
         for card in soup.find_all(name='a', class_='card-img-hover')[:self.max_topics + 1]:
             new_scrapy = Scrapy('topic', scrapy.author, card.get('title'), card.get('href'))
             if new_scrapy not in self.scraped:
                 self.to_crawl.put(new_scrapy)
-        self.scraped.add(scrapy)
 
-    @print_status
+    @update_result
     def parse_images(self, scrapy, html):
         soup = BeautifulSoup(markup=html.text, features='html.parser')
         for div in soup.find_all(name='div', class_='reveal-work-wrap text-center'):
@@ -137,9 +152,8 @@ class MultiThreadScraper():
             new_scrapy = Scrapy('image', scrapy.author, scrapy.title, url)
             if new_scrapy not in self.scraped:
                 self.to_crawl.put(new_scrapy)
-        self.scraped.add(scrapy)
 
-    @print_status
+    @update_result
     def save_image(self, scrapy, html):
         path = os.path.join(self.directory, self.convert_to_safe_filename(scrapy.title))
         self.mkdirs_if_not_exist(path)
@@ -149,13 +163,14 @@ class MultiThreadScraper():
             name = uuid4().hex + '.jpg'
         with open(os.path.join(path, name), 'wb') as fi:
             fi.write(html.content)
-        self.scraped.add(scrapy)
 
     def post_scrape_callback(self, res):
-        result = res.result()
-        if not result or result[1].status_code != 200:
+        scrapy, response = res.result()
+
+        if (not response) or (response.status_code != 200):
+            self.failed.add(scrapy)
             return
-        scrapy, response = result
+
         if scrapy.type == 'page':
             self.parse_topics(scrapy, response)
         elif scrapy.type == 'topic':
@@ -164,25 +179,30 @@ class MultiThreadScraper():
             self.save_image(scrapy, response)
 
     def run_scraper(self):
-        futures = []
+        futures = {}
         while True:
             try:
-                target_scrapy = self.to_crawl.get(timeout=3)
+                target_scrapy = self.to_crawl.get(timeout=7)
                 if target_scrapy not in self.scraped:
-                    # self.scraped.add(target_scrapy)
                     job = self.pool.submit(self.scrape_page, target_scrapy)
                     job.add_done_callback(self.post_scrape_callback)
-                    futures.append(job)
+                    futures[job] = target_scrapy
             except Empty:
                 break
             except Exception as e:
-                print(e)
+                print('\n\n', e)
                 continue
+
         wait(futures)
-        sleep(5)
         saved_images = len([1 for s in self.scraped if s.type == "image"])
+        failed_images = len([1 for s in self.failed if s.type == "image"])
+        self.show_task_status()
+        if saved_images or failed_images:
+            print(f'\n\nImages download success: {saved_images} \tImages download failed: {failed_images}')
+        else:
+            print('\n\nNo images to download.')
         if saved_images:
-            print(f'\n\nSaved {saved_images} images to {self.directory}.')
+            print(f'Saved {saved_images} images to {self.directory}.')
 
 
 @click.command()
@@ -191,7 +211,7 @@ class MultiThreadScraper():
 @click.option('-d', '--directory', 'dir', help='Directory to save images.')
 @click.option('-p', '--max-pages', 'max_pages', type=int, help='Max pages to parse.')
 @click.option('-t', '--max-topics', 'max_topics', type=int, help='Max topics per page to parse.')
-@click.option('-w', '--max-workers', 'max_workers', default=12, show_default=True,
+@click.option('-w', '--max-workers', 'max_workers', default=20, show_default=True,
               type=int, help='Max thread workers.')
 def command(id, name, dir, max_pages, max_topics, max_workers):
     """Use multi-threaded to download images from https://www.zcool.com.cn in bulk by username or ID."""
